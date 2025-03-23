7ec9b6d1974c3dec1a17e2bc14664aaf9b143b99
hjhjhjhhjhjhhh <binglin.cs10@nycu.edu.tw>
2025-03-23 12:31:18 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
+
 # Visual-Recognition-using-Deep-Learning-2025-Spring-HW1
+
+| StudentID |   110550067 |
+| --------- | :-----|
+| **Name**  |    **<font face = STKaiti>簡秉霖** |
+
+## Introduction
+
+In this assignment, we tackle image classification with 100 classes using a dataset of 20,724 training images, 300 validation images, and 2,344 test images.I use the ResNeXt architecture with pretrained weights from Torchvision. By carefully preprocessing the data and fine-tuning hyperparameters, I achieve an impressive 95% accuracy on the public set, demonstrating the strength of the ResNeXt model.
+
+## How to install
+- Python version: 3.10
+
+- Download required packages.
+  ` pip install requirements.txt`
+- Check the official Pytorch website to download torch-related packages, ensuring you select the correct CUDA version (11.8 in this case).
+`
+pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu118
+`
+
+## How to run
+- For training: ` python train.py`
+  The model weights will be store at `resnext_best.pth`
+  
+- For testing: `python test.py`
+
+## Performance snapshot
+![The public prediction score](snapshot/performance.jpg)
---
+++ train.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = {
+    "train": transforms.Compose([
+        transforms.Resize(500),
+        transforms.RandomCrop(400),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]),
+    "val": transforms.Compose([
+        transforms.Resize(500),
+        transforms.CenterCrop(400),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = "../vrdl_hw1/data" # put the correct path here
+train_dataset = datasets.ImageFolder(root=f"{data_dir}/train", transform=transform["train"])
+val_dataset = datasets.ImageFolder(root=f"{data_dir}/val", transform=transform["val"])
+idx_to_class = {v: k for k, v in train_dataset.class_to_idx.items()}
+
+batch_size = 16
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+model = models.resnext50_32x4d(weights='DEFAULT')
+
+# Modify the fully connected layer to match the number of classes (100)
+num_ftrs = model.fc.in_features
+model.fc = nn.Sequential(
+    nn.Dropout(p=0.5),
+    nn.Linear(num_ftrs, 100)
+)
+
+model = model.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.00025, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="max", factor=0.1, patience=5, verbose=True)
+
+num_epochs = 20
+best_val_acc = 0.0
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        correct += (predicted == labels).sum().item()
+        total += labels.size(0)
+        
+    current_lr = optimizer.param_groups[0]['lr']
+    train_acc = 100 * correct / total
+    print(f"Epoch [{epoch+1}/{num_epochs}], lr: {current_lr:.6f}, Loss: {running_loss/len(train_loader):.4f}, Accuracy: {train_acc:.2f}%")
+
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            val_loss += loss.item()
+            _, predicted = outputs.max(1)
+            correct += (predicted == labels).sum().item()
+            total += labels.size(0)
+
+    val_acc = 100 * correct / total
+    print(f"Validation Loss: {val_loss/len(val_loader):.4f}, Accuracy: {val_acc:.2f}%")
+
+    scheduler.step(val_acc)
+
+    if val_acc > best_val_acc:
+        best_val_acc = val_acc
+        torch.save(model.state_dict(), "resnext_best.pth")
+        print(f"New best model saved with accuracy: {best_val_acc:.2f}%")
+
+
+

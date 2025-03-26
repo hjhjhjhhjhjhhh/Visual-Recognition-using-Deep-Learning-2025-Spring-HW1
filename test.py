55a25ef1074b659364e931d4a9036c34e6985851
hjhjhjhhjhjhhh <binglin.cs10@nycu.edu.tw>
2025-03-26 13:47:52 +0800

    modify code to PEP8 style

--- test.py
+++ test.py
@@ -9,36 +9,35 @@ import pandas as pd
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-transform = transforms.Compose([
-    transforms.Resize(500),
-    transforms.CenterCrop(400),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+transform = transforms.Compose(
+    [
+        transforms.Resize(500),
+        transforms.CenterCrop(400),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
-data_dir = "../vrdl_hw1/data" ## put the correct path here
+data_dir = "../vrdl_hw1/data"  ## put the correct path here
 val_dataset = datasets.ImageFolder(root=f"{data_dir}/val", transform=transform)
 idx_to_class = {v: k for k, v in val_dataset.class_to_idx.items()}
 
-model = models.resnext50_32x4d(weights='DEFAULT')
+model = models.resnext50_32x4d(weights="DEFAULT")
 
 # Modify the fully connected layer to match the number of classes (100)
 num_ftrs = model.fc.in_features
-model.fc = nn.Sequential(
-    nn.Dropout(p=0.5),
-    nn.Linear(num_ftrs, 100)
-)
+model.fc = nn.Sequential(nn.Dropout(p=0.5), nn.Linear(num_ftrs, 100))
 
 model.load_state_dict(torch.load("resnext_best.pth", map_location=device))
 model.to(device)
 model.eval()
 
-image_list = sorted(os.listdir(os.path.join(data_dir, 'test')))
+image_list = sorted(os.listdir(os.path.join(data_dir, "test")))
 predictions = []
 
 for img_name in image_list:
-    if img_name.lower().endswith((".png", ".jpg", ".jpeg")): 
-        img_path = os.path.join(os.path.join(data_dir, 'test'), img_name)
+    if img_name.lower().endswith((".png", ".jpg", ".jpeg")):
+        img_path = os.path.join(os.path.join(data_dir, "test"), img_name)
         image = Image.open(img_path).convert("RGB")
         image = transform(image).unsqueeze(0).to(device)
 
@@ -53,4 +52,3 @@ output_csv = "prediction.csv"
 df = pd.DataFrame(predictions, columns=["image_name", "pred_label"])
 df.to_csv(output_csv, index=False)
 print("Prediction finish")
-
--- train.py
+++ train.py
@@ -5,25 +5,32 @@ from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device is ", device)
 
 transform = {
-    "train": transforms.Compose([
-        transforms.Resize(500),
-        transforms.RandomCrop(400),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ]),
-    "val": transforms.Compose([
-        transforms.Resize(500),
-        transforms.CenterCrop(400),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ]),
+    "train": transforms.Compose(
+        [
+            transforms.Resize(600),
+            transforms.RandomCrop(500),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    ),
+    "val": transforms.Compose(
+        [
+            transforms.Resize(600),
+            transforms.CenterCrop(500),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    ),
 }
 
-data_dir = "../vrdl_hw1/data" # put the correct path here
-train_dataset = datasets.ImageFolder(root=f"{data_dir}/train", transform=transform["train"])
+data_dir = "../vrdl_hw1/data"  # put the correct path here
+train_dataset = datasets.ImageFolder(
+    root=f"{data_dir}/train", transform=transform["train"]
+)
 val_dataset = datasets.ImageFolder(root=f"{data_dir}/val", transform=transform["val"])
 idx_to_class = {v: k for k, v in train_dataset.class_to_idx.items()}
 
@@ -31,19 +38,18 @@ batch_size = 16
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-model = models.resnext50_32x4d(weights='DEFAULT')
+model = models.resnext50_32x4d(weights="DEFAULT")
 
 # Modify the fully connected layer to match the number of classes (100)
 num_ftrs = model.fc.in_features
-model.fc = nn.Sequential(
-    nn.Dropout(p=0.5),
-    nn.Linear(num_ftrs, 100)
-)
+model.fc = nn.Sequential(nn.Dropout(p=0.5), nn.Linear(num_ftrs, 100))
 
 model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.00025, weight_decay=1e-4)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="max", factor=0.1, patience=5, verbose=True)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer, mode="max", factor=0.1, patience=5, verbose=True
+)
 
 num_epochs = 20
 best_val_acc = 0.0
@@ -54,7 +60,6 @@ for epoch in range(num_epochs):
     correct = 0
     total = 0
 
-    
     for images, labels in train_loader:
         images, labels = images.to(device), labels.to(device)
 
@@ -68,10 +73,12 @@ for epoch in range(num_epochs):
         _, predicted = outputs.max(1)
         correct += (predicted == labels).sum().item()
         total += labels.size(0)
-        
-    current_lr = optimizer.param_groups[0]['lr']
+
+    current_lr = optimizer.param_groups[0]["lr"]
     train_acc = 100 * correct / total
-    print(f"Epoch [{epoch+1}/{num_epochs}], lr: {current_lr:.6f}, Loss: {running_loss/len(train_loader):.4f}, Accuracy: {train_acc:.2f}%")
+    print(
+        f"Epoch [{epoch+1}/{num_epochs}], lr: {current_lr:.6f}, Loss: {running_loss/len(train_loader):.4f}, Accuracy: {train_acc:.2f}%"
+    )
 
     model.eval()
     val_loss = 0.0
@@ -98,6 +105,4 @@ for epoch in range(num_epochs):
         best_val_acc = val_acc
         torch.save(model.state_dict(), "resnext_best.pth")
         print(f"New best model saved with accuracy: {best_val_acc:.2f}%")
-
-
-
+print("Training Process done!")
